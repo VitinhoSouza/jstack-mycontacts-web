19c8e835ba113298a3dccadf5954618e008c7ee8
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2024-07-07 12:52:18 -0300

    feat: organizing project and creating ContactsList page

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061FC',
+      dark: '#3346F0',
+    },
+    gray: {
+      900: '#222',
+      200: '#BCBCBC',
+    }
+  }
+}
---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import { ThemeProvider } from 'styled-components';
+
+import Header from '../Header';
+
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
+
+import { Container } from './styles';
+import ContactsList from '../ContactsList';
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Container>
+        <Header />
+        <ContactsList />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
