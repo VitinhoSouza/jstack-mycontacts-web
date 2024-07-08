842d553c39344e4057f39c663e40b3520b606ed0
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2024-07-08 13:52:47 -0300

    feat: implementing input error status in input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,14 +5,11 @@ import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
 
 import { Card, Container, Header, ListContainer, InputSearchContainer } from "./styles";
-import Loader from "../../components/Loader";
 
 export default function Home() {
 
   return (
     <Container>
-
-      <Loader />
 
       <InputSearchContainer>
         <input type='text' placeholder='Pesquise pelo nome...' />
